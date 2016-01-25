3d5343c6feafcfdab29586e74590aa7fa5d5c945
michal-ciechan <lndcobra@hotmail.com>
2016-01-25 23:28:29 +0000

    Added CsvCompare

---
+++ CsvCompare/source/CsvCompare/Botstrapper/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace CsvCompare.Botstrapper
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
